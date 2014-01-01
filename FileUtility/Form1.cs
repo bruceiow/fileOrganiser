a742496e7696cc267d182574c73ff574793795bf
bruceiow <bruceiow@gmail.com>
2014-01-01 22:17:05 +0000

    refactor to check for existence of datetaken as opposed to using catch

--- FileUtility/Form1.cs
+++ FileUtility/Form1.cs
@@ -15,6 +15,18 @@ namespace FileUtility
 {
     public partial class Form1 : Form
     {
+        string sdate;
+        string secondhalf;
+        string firsthalf;
+        string takenMonth;
+        string takenYear;
+        string takenFolder;
+        string newFileSerialised;
+        string outputResult;
+        bool exist;
+        int fileCount = 0;
+        int exceptionCount = 0;
+        DateTime dtaken;
         string outputsuffix;
         public Form1()
         {
@@ -26,23 +38,11 @@ namespace FileUtility
             button1.Enabled = false;
             lblResult.Text = "Working...Please Wait";
             string directory = directorySelection.Text;
-          
-            string sdate;
-            string secondhalf;
-            string firsthalf;
-            string takenMonth;
-            string takenYear;
-            string takenFolder;
-            string newFileSerialised;
-            string outputResult;
-            bool exist;
-            int fileCount = 0;
-            int exceptionCount = 0;
-            DateTime dtaken;
-
             try
             {
-                var filesInDir = Directory.EnumerateFiles(directory, "*.jpg", SearchOption.AllDirectories);
+                string supportedExtensions = "*.jpg,*.gif,*.png,*.bmp,*.jpe,*.jpeg,*.wmf,*.emf,*.xbm,*.ico,*.eps,*.tif,*.tiff";
+
+                var filesInDir = Directory.EnumerateFiles(directory,"*", SearchOption.AllDirectories).Where(s => supportedExtensions.Contains(Path.GetExtension(s).ToLower()));
                 int countFiles = filesInDir.Count();
                 if (countFiles >0)
                 {
@@ -58,13 +58,19 @@ namespace FileUtility
                         lblResult.Text = "Currently Processing: " + fileCount.ToString() + " of " +countFiles;
                         lblResult.Refresh();
 
-                        //If Image then group by date taken
                         Image myImage = Image.FromFile(file);
                         try
                         {
-                            PropertyItem propItem = myImage.GetPropertyItem(306);
-                          
-                            sdate = Encoding.UTF8.GetString(propItem.Value).Trim();
+                           if (myImage.PropertyIdList.Any(x => x == 36867))
+                           {
+                               PropertyItem propItem = myImage.GetPropertyItem(36867);
+                               sdate = Encoding.UTF8.GetString(propItem.Value).Trim();
+                           }
+                           else
+                           {  
+                               sdate = File.GetCreationTime(file).ToString();
+                           }
+                            myImage.Dispose();
                             secondhalf = sdate.Substring(sdate.IndexOf(" "), (sdate.Length - sdate.IndexOf(" ")));
                             firsthalf = sdate.Substring(0, 10);
                             firsthalf = firsthalf.Replace(":", "-");
@@ -78,7 +84,6 @@ namespace FileUtility
                             outputResult = outputDirectory.Text + takenFolder + @"\" + newFileSerialised;
                             if (Directory.Exists(outputDirectory.Text + takenFolder))
                             {
-
                                 exist = File.Exists(outputResult);
 
                                 if (exist)
@@ -93,22 +98,13 @@ namespace FileUtility
                                 Directory.CreateDirectory(outputDirectory.Text + takenFolder);
                                 File.Copy(file, outputResult);
                             }
-
-
                         }
                         catch
                         {
-                            //Anything that isnt an image will get filed by created date..
-                            dtaken = File.GetCreationTime(file);
-                            takenMonth = dtaken.ToString("MMM", CultureInfo.InvariantCulture);
-                            takenYear = dtaken.Year.ToString();
-                            takenFolder = takenMonth + " " + takenYear;
-                            outputsuffix = Path.GetExtension(file);
-                            newFileSerialised = dtaken.ToString().Replace(@"/", "").Replace(":", "").Replace(" ", "") + outputsuffix;
                         }
                         lblResult.Text = "processed";
                         lblResult.Refresh();
-                        myImage.Dispose();
+                       
                     }
 
                     lblResult.Text = "Files Processed:" + fileCount + " Exceptions:" + exceptionCount;
@@ -136,12 +132,11 @@ namespace FileUtility
             outputsuffix = Path.GetExtension(outputFile);
             if (File.Exists(outputResult))
             {
-               
+              
                 string checkFileNumber = Path.GetFileNameWithoutExtension(outputFile);
                 incrimentCounter = Directory.EnumerateFiles(outputDirectory, "*" + checkFileNumber + "*").Count();
-                incrimentCounter++;
-                outputResult = outputResult.ToLower().Replace(".jpg", "");
-                outputResult = outputResult + "_" + incrimentCounter + outputsuffix;
+            //    incrimentCounter++;
+                outputResult = outputDirectory+@"\"+ Path.GetFileNameWithoutExtension(outputFile) + "_" + incrimentCounter + outputsuffix;
                 return outputResult;
             }
             return outputResult;
